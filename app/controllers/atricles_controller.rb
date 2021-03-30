da93d7be0f56ea4f0c216590ddb4a0de61808659
Michael Epelboim <sdrmike@outlook.com>
2021-03-30 16:26:55 +0800

    completed the message board, needs styling

---
+++ app/controllers/atricles_controller.rb
@@ -0,0 +1,4 @@
+class AtriclesController < ApplicationController
+ 
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# title: Faker::Book.title
+# content: Faker::Hacker.say_something_smart
+
+10.times do
+  Article.create!(
+    title: Faker::Book.title,
+    content: Faker::Hacker.say_something_smart
+  )
+end
